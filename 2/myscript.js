db7aef08f9414e8602f78a091c357e4c3e39d5bd
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-21 18:15:07 +0200

    DADI - creazione variabili e dichiarazione valori

--- 2/myscript.js
+++ 2/myscript.js
@@ -1,1 +1,11 @@
-document.getElementById('id').innerHTML = "hei";
+// Gioco dei dadi, chi fa di più vince;
+
+var numA, numB;
+
+numA = Math.floor((Math.random() * 6) + 1);
+
+console.log(numA);
+
+numB = Math.floor((Math.random() * 6) + 1);
+
+console.log(numB);
