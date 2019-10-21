4892674b1fba7950e4bb2d988510ebdb4db52a6e
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-21 18:46:33 +0200

    DADI - Variabile sottrazione

--- 2/myscript.js
+++ 2/myscript.js
@@ -1,6 +1,6 @@
 // Gioco dei dadi, chi fa di più vince;
 
-var numA, numB;
+var numA, numB, sottrazione
 
 numA = Math.floor((Math.random() * 6) + 1);
 
@@ -9,3 +9,13 @@ console.log(numA);
 numB = Math.floor((Math.random() * 6) + 1);
 
 console.log(numB);
+
+sottrazione = numA - numB;
+
+console.log('uguali')
+
+if(sottrazione > 0) {
+  console.log(numA + ' > ' + numB + ' : Vince il giocatore A');
+} else if (sottrazione < 0) {
+  console.log(numA + ' < ' + numB + ' : Vince il giocatore B');
+}
